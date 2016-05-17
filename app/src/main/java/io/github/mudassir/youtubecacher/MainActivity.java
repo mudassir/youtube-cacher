ce7fe1aa5c77b006a39022b5411efa9b5028915d
mudassir <mudassir@users.noreply.github.com>
2016-05-17 15:51:44 -0400

    Adding vget library, getting dexError

--- app/src/main/java/io/github/mudassir/youtubecacher/MainActivity.java
+++ app/src/main/java/io/github/mudassir/youtubecacher/MainActivity.java
@@ -1,7 +1,13 @@
 package io.github.mudassir.youtubecacher;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.axet.vget.VGet;
+
+import java.io.File;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		try {
+			// ex: http://www.youtube.com/watch?v=Nj6PFaDmp6c
+			String url = "https://www.youtube.com/watch?v=-YGDyPAwQz0";
+			// ex: "/Users/axet/Downloads"
+			String path = Environment.getExternalStorageDirectory().getPath() + "/Download/";
+			VGet v = new VGet(new URL(url), new File(path));
+			v.download();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
