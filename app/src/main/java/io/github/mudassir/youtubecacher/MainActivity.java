e4ab3066b8f4adfd682d4f7d76484fe2a725a520
mudassir <mudassir@users.noreply.github.com>
2016-05-25 14:14:39 -0400

    Workaround for gradle error by enabling multiDex

--- app/src/main/java/io/github/mudassir/youtubecacher/MainActivity.java
+++ app/src/main/java/io/github/mudassir/youtubecacher/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.mudassir.youtubecacher;
 
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,15 +17,22 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		try {
-			// ex: http://www.youtube.com/watch?v=Nj6PFaDmp6c
-			String url = "https://www.youtube.com/watch?v=-YGDyPAwQz0";
-			// ex: "/Users/axet/Downloads"
-			String path = Environment.getExternalStorageDirectory().getPath() + "/Download/";
-			VGet v = new VGet(new URL(url), new File(path));
-			v.download();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				try {
+					// ex: http://www.youtube.com/watch?v=Nj6PFaDmp6c
+					String url = "https://www.youtube.com/watch?v=-YGDyPAwQz0";
+					// ex: "/Users/axet/Downloads"
+					String path = Environment.getExternalStorageDirectory().getPath() + "/Download/";
+					VGet v = new VGet(new URL(url), new File(path));
+					v.download();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+
+				return null;
+			}
+		}.execute();
 	}
 }
